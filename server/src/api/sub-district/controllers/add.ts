97e29e60f3e6c8714070242015240eee8ba5f716
JaJoJi <TheJ0918478513@gmail.com>
2024-11-10 22:49:43 +0700

    เช็คตำบล ID

--- server/src/api/sub-district/controllers/add.ts
+++ server/src/api/sub-district/controllers/add.ts
@@ -1,4 +1,5 @@
 import { Context } from 'koa';
+import { ID } from '@strapi/types/dist/data';
 
 export default {
   async createsubdistricts(ctx: Context) {
@@ -47,4 +48,69 @@ export default {
       ctx.internalServerError('Failed to process subdistricts');
     }
   },
+
+
+  async get_id(ctx: Context) {
+    console.log(ctx.request.body)
+    let get_province_id: ID
+    let get_District_id: ID
+    let province_name : String
+    let Discrit_name : String
+    const { Province_n, District_n, S_District_n } = ctx.request.body.data;
+    console.log(Province_n, District_n, S_District_n)
+    
+    if (!Province_n || !District_n || !S_District_n) {
+      return ctx.badRequest('เอาข้อมูลมาให้ครบครับน้อง');
+    }
+
+    try {
+      const Get_Province = await strapi.entityService.findMany('api::province.province', {
+        filters: {
+          name_th: Province_n
+        },
+      });
+      
+      if (Get_Province.length === 0) {
+        return ctx.badRequest('ไม่พบจังหวัดที่มีชื่อดังกล่าว');
+      }
+
+      province_name = Get_Province[0].name_th
+      get_province_id = Get_Province[0].id;  
+
+      const Get_Districts = await strapi.entityService.findMany('api::district.district', {
+        filters: {
+          name_th: District_n,  
+          province_id: {id : get_province_id }  
+        },
+      });
+      
+      if (Get_Districts.length === 0) {
+        return ctx.badRequest('ไม่พบเขตที่ตรงกับชื่อในจังหวัดนี้');
+      }
+      Discrit_name = Get_Districts[0].name_th
+      get_District_id = Get_Districts[0].id;  
+
+      const Get_s_Districts = await strapi.entityService.findMany('api::sub-district.sub-district', {
+        filters: {
+          name_th: S_District_n,  
+          amphure_id: {id : get_District_id }  
+        },
+      });
+      
+      if (Get_Districts.length === 0) {
+        return ctx.badRequest('ไม่พบเขตที่ตรงกับชื่อในจังหวัดนี้');
+      }
+
+      ctx.send({
+        district: Get_s_Districts[0],
+        province: province_name,
+        district_name: Discrit_name
+      }, 200); 
+      
+
+    } catch (error) {
+      console.log("เกิดข้อผิดพลาด:", error);
+      ctx.internalServerError('เกิดข้อผิดพลาดในการดึงข้อมูล');
+    }
+}
 };
