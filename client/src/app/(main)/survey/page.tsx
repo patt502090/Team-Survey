42805773e0fc122cbbc903c4ecc3148391da00b8
ปรเมษ แก้วอุบล <keawubolporamet505@gmail.com>
2024-11-10 18:28:30 +0700

    PostCustomer

--- client/src/app/(main)/survey/page.tsx
+++ client/src/app/(main)/survey/page.tsx
@@ -4,21 +4,30 @@ import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Camera from "@/components/Survey/Camera";
 import CustomerInfo from "@/components/Survey/CustomerInfo";
 import CheckInfo from "@/components/Survey/CheckInfo";
 import { OCRResponse } from "../../../modules/ocrSchema";
 import { CustomerProps } from "@/modules/customerSchema";
+import { useRouter } from "next/navigation";
+import ax from "@/conf/ax";
+import conf from "@/conf/main";
+import { AuthContext } from "@/contexts/Auth.context";
 
 const steps = ["ถ่ายรูป", "ข้อมูล", "ตรวจสอบข้อมูล"];
 
 export default function HorizontalLinearAlternativeLabelStepper() {
+  const router = useRouter();
+  const context = useContext(AuthContext);
+  const ContextState = context ? context.state : null;
+  console.log(ContextState);
   const [activeStep, setActiveStep] = useState(0);
   const [customerData, setCustomerData] = useState<OCRResponse | null>(null);
   const [customerDatatoCheck, setCustomerDatatoCheck] =
     useState<CustomerProps | null>(null);
   const [isFinished, setIsFinished] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
 
   const handleNext = () => {
     if (activeStep === steps.length - 1) {
@@ -36,6 +45,34 @@ export default function HorizontalLinearAlternativeLabelStepper() {
   };
   const handleCustomerDataUpdate = (updatedData: CustomerProps) => {
     setCustomerDatatoCheck(updatedData);
+  };
+
+  const PostCustomer = async () => {
+    try {
+      if (!customerDatatoCheck) return;
+
+      const requestData = {
+        data: {
+          Id_Number: customerDatatoCheck.Id_Number,
+          NameTitle: customerDatatoCheck.NameTitle,
+          Name_Th: customerDatatoCheck.Name_Th,
+          Name_Eng: customerDatatoCheck.Name_Eng,
+          Address: /*customerDatatoCheck.Address*/ "2024-11-10",
+          birthdate: customerDatatoCheck.birthdate,
+          // Assessor: ContextState?.user?.documentId, ตรงนี้ๆ
+          // business_with: ContextState?.user?.r,
+          estimate: customerDatatoCheck.estimate,
+          sub_district: customerDatatoCheck.Address,
+          Pic: customerDatatoCheck.Pic, // อาจต้องคอมเม้นตรงนี้ไปก่อนถ้าจะใช้ ax.post
+        },
+      };
+      console.log(requestData);
+
+      // await ax.post(`${conf.apiUrlPrefix}/customers`, requestData);
+      setShowPopup(true);
+    } catch (error) {
+      console.error("Error posting customer data:", error);
+    }
   };
 
   return (
@@ -69,7 +106,7 @@ export default function HorizontalLinearAlternativeLabelStepper() {
             {customerData && (
               <CustomerInfo
                 customerData={customerData}
-                onDataUpdate={handleCustomerDataUpdate} 
+                onDataUpdate={handleCustomerDataUpdate}
               />
             )}
             {/* <CustomerInfo /> */}
@@ -103,7 +140,10 @@ export default function HorizontalLinearAlternativeLabelStepper() {
             <div className="mt-auto flex space-x-4 justify-between px-4 mb-4">
               {activeStep <= steps.length - 1 && (
                 <button
-                  onClick={handleNext}
+                  onClick={() => {
+                    handleNext();
+                    PostCustomer();
+                  }}
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full sm:w-auto mx-auto"
                 >
                   ถัดไป
@@ -118,6 +158,22 @@ export default function HorizontalLinearAlternativeLabelStepper() {
                 </button>
               )}
             </div>
+            {showPopup && (
+              <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                <div className="bg-white p-6 rounded-lg text-center">
+                  <p>Created successfully</p>
+                  <button
+                    onClick={() => {
+                      setShowPopup(false);
+                      router.push("/dashboard");
+                    }}
+                    className="mt-4 bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Ok, Thank!
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
         )}
       </div>
--- client/src/components/Survey/CheckInfo.tsx
+++ client/src/components/Survey/CheckInfo.tsx
@@ -7,38 +7,44 @@ interface CheckInfoProps {
 
 const CheckInfo: React.FC<CheckInfoProps> = ({ customerData }) => {
   return (
-    <div>
-      <div className="flex flex-col items-center p-2 space-y-4">
-        <h2 className="text-xl font-bold">ขั้นตอนที่ 3 ตรวจสอบข้อมูล</h2>
+    <div className="flex flex-col items-center p-2 space-y-2">
+      <div className="flex flex-col items-center space-y-4">
+        <h2 className="text-xl font-bold text-center">
+          ขั้นตอนที่ 3 ตรวจสอบข้อมูล
+        </h2>
       </div>
-      <div>
-        <h2>ข้อมูลลูกค้า</h2>{" "}
-        <img
-          className="w-12 h-12 mr-4 object-cover"
-          src={customerData.Pic}
-          alt="Avatar Upload"
-        />
-        <p>
-          <strong>เลขบัตรประชาชน:</strong> {customerData.Id_Number}
-        </p>
-        <p>
-          <strong>คำนำหน้าชื่อ:</strong> {customerData.NameTitle}
-        </p>
-        <p>
-          <strong>ชื่อ-นามสกุล ภาษาไทย:</strong> {customerData.Name_Th}
-        </p>
-        <p>
-          <strong>ชื่อ-นามสกุล ภาษาอังกฤษ:</strong> {customerData.Name_Eng}
-        </p>
-        <p>
-          <strong>ที่อยู่:</strong> {customerData.Address}
-        </p>
-        <p>
-          <strong>วันเกิด:</strong> {customerData.birthdate}
-        </p>
-        <p>
-          <strong>การประเมินผล:</strong> {customerData.estimate}
-        </p>
+      <div className="space-y-4 mt-4">
+        <h3 className="text-lg font-semibold">ข้อมูลลูกค้า</h3>
+        <div className="flex items-center space-x-4">
+          <img
+            className="w-16 h-16 object-cover rounded-full"
+            src={customerData.Pic}
+            alt="Avatar Upload"
+          />
+          <div className="space-y-2">
+            <p>
+              <strong>เลขบัตรประชาชน:</strong> {customerData.Id_Number}
+            </p>
+            <p>
+              <strong>คำนำหน้าชื่อ:</strong> {customerData.NameTitle}
+            </p>
+            <p>
+              <strong>ชื่อ-นามสกุล ภาษาไทย:</strong> {customerData.Name_Th}
+            </p>
+            <p>
+              <strong>ชื่อ-นามสกุล ภาษาอังกฤษ:</strong> {customerData.Name_Eng}
+            </p>
+            <p>
+              <strong>ที่อยู่:</strong> {customerData.Address}
+            </p>
+            <p>
+              <strong>วันเกิด:</strong> {customerData.birthdate}
+            </p>
+            <p>
+              <strong>การประเมินผล:</strong> {customerData.estimate}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
