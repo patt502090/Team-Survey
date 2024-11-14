a10447ff40745a7f5d77f248c6f4c6a3c79be48a
JaJoJi <TheJ0918478513@gmail.com>
2024-11-14 16:25:44 +0700

    Update filter

--- server/src/api/customer/controllers/check.ts
+++ server/src/api/customer/controllers/check.ts
@@ -5,85 +5,34 @@ export default {
     const S_districtData = ctx.request.body;
 
     try {
-        const Get_s_Districts = await strapi.entityService.findMany('api::customer.customer', {
-            filters: {
-              sub_district: { id: S_districtData.data.S_district_id },
-            },
-          });
-          console.log(Get_s_Districts)
-         
-          const statusCount = {
-            green: 0,
-            yellow: 0,
-            red: 0,
-          };
-          Get_s_Districts.forEach(customer => {
-            const estimateStatus = customer.estimate; 
-            if (estimateStatus.toLowerCase() === 'green') {
-              statusCount.green++;
-            } else if (estimateStatus.toLowerCase() === 'yellow') {
-              statusCount.yellow++;
-            } else if (estimateStatus.toLowerCase() === 'red') {
-              statusCount.red++;
-            }
-          });
-          ctx.send({
-            greenCount: statusCount.green,
-            yellowCount: statusCount.yellow,
-            redCount: statusCount.red,
-          }, 200);
-
+      let Get_s_Districts: any
+      if (S_districtData.data.team_id !== null) {
+        const Get_team = await strapi.entityService.findOne('api::team.team', S_districtData.data.team_id);
+        if (!Get_team) {
+          return ("ไม่เจอทีมอะคุณ Id ผิดป่าว")
         }
-          catch (error) {
-      console.error('Error creating Provinces:', error);
-      ctx.internalServerError('Failed to create Provinces');
-    }
-  },
-  async estimate_checked_d(ctx: Context) {
-    const S_districtData = ctx.request.body;
-    try {
-      const Get_District = await strapi.entityService.findOne('api::district.district', S_districtData.data.district_id, {
-        populate: {
-          sub_districts: {
-            populate: {
-              customers: true, 
-            },
-          },
-        },
-      }) as { 
-        id: string, 
-        documentId: string, 
-        locale?: string, 
-        createdAt?: string, 
-        updatedAt?: string, 
-        publishedAt?: string, 
-        name_en?: string, 
-        name_th?: string,
-        sub_districts: Array<{
-          id: string,
-          documentId: string,
-          name_th: string,
-          name_en: string,
-          customers: any[] 
-        }>
-      };
-  
-      if (!Get_District) {
-        return ctx.badRequest('ไม่พบข้อมูล District ที่ต้องการ');
+        Get_s_Districts = await strapi.entityService.findMany('api::customer.customer', {
+          filters: {
+            sub_district: { id: S_districtData.data.S_district_id },
+            business_with: { id: Get_team.id }
+          }
+        });
+        console.log(Get_s_Districts)
+      } else {
+        Get_s_Districts = await strapi.entityService.findMany('api::customer.customer', {
+          filters: {
+            sub_district: { id: S_districtData.data.S_district_id },
+          }
+        });
       }
-      const allCustomers: any[] = [];
-      Get_District.sub_districts.forEach(subDistrict => {
-        if (subDistrict.customers && subDistrict.customers.length > 0) {
-          allCustomers.push(...subDistrict.customers); 
-        }
-      });
+
       const statusCount = {
         green: 0,
         yellow: 0,
         red: 0,
       };
-      allCustomers.forEach(customer => {
-        const estimateStatus = customer.estimate; 
+      Get_s_Districts.forEach(customer => {
+        const estimateStatus = customer.estimate;
         if (estimateStatus.toLowerCase() === 'green') {
           statusCount.green++;
         } else if (estimateStatus.toLowerCase() === 'yellow') {
@@ -96,22 +45,124 @@ export default {
         greenCount: statusCount.green,
         yellowCount: statusCount.yellow,
         redCount: statusCount.red,
+        total_customer: statusCount.green + statusCount.yellow + statusCount.red
       }, 200);
-      }
-          catch (error) {
+
+    }
+    catch (error) {
       console.error('Error creating Provinces:', error);
       ctx.internalServerError('Failed to create Provinces');
     }
-  },async estimate_checked_p(ctx: Context) {
+  },
+  async estimate_checked_d(ctx: Context) {
+    const S_districtData = ctx.request.body;
+    try {
+      const Get_District = await strapi.entityService.findOne('api::district.district', S_districtData.data.district_id, {
+        populate: {
+          sub_districts: {
+            populate: {
+              customers: {
+                populate: {
+                  business_with: true
+                }
+              }
+            },
+          },
+        },
+      }) as {
+        id: string,
+        documentId: string,
+        locale?: string,
+        createdAt?: string,
+        updatedAt?: string,
+        publishedAt?: string,
+        name_en?: string,
+        name_th?: string,
+        sub_districts: Array<{
+          id: string,
+          documentId: string,
+          name_th: string,
+          name_en: string,
+          customers: Array<{
+            id: string,
+            documentId: string,
+            estimate: string,
+            business_with: {
+              id: string,
+              documentId: string
+            }
+          }>
+        }>
+      };
+
+      if (!Get_District) {
+        return ctx.badRequest('ไม่พบข้อมูล District ที่ต้องการ');
+      }
+
+      const allCustomers: any[] = [];
+      if (S_districtData.data.team_id === null) {
+        Get_District.sub_districts.forEach(subDistrict => {
+          if (subDistrict.customers && subDistrict.customers.length > 0) {
+            allCustomers.push(...subDistrict.customers);
+          }
+        });
+      } else {
+        const Get_team = await strapi.entityService.findOne('api::team.team', S_districtData.data.team_id);
+        if (!Get_team) {
+          return ("ไม่เจอทีมอะคุณ Id ผิดป่าว")
+        }
+        Get_District.sub_districts.forEach(subDistrict => {
+          if (subDistrict.customers && subDistrict.customers.length > 0) {
+            subDistrict.customers.forEach(customer => {
+              console.log(customer.business_with)
+              if (customer.business_with && customer.business_with.id === Get_team.id) {
+                allCustomers.push(customer);
+              }
+            });
+          }
+        });
+      }
+      console.log(allCustomers)
+      const statusCount = {
+        green: 0,
+        yellow: 0,
+        red: 0,
+      };
+      allCustomers.forEach(customer => {
+        const estimateStatus = customer.estimate;
+        if (estimateStatus.toLowerCase() === 'green') {
+          statusCount.green++;
+        } else if (estimateStatus.toLowerCase() === 'yellow') {
+          statusCount.yellow++;
+        } else if (estimateStatus.toLowerCase() === 'red') {
+          statusCount.red++;
+        }
+      });
+      ctx.send({
+        greenCount: statusCount.green,
+        yellowCount: statusCount.yellow,
+        redCount: statusCount.red,
+        total_customer: statusCount.green + statusCount.yellow + statusCount.red
+      }, 200);
+    }
+    catch (error) {
+      console.error('Error creating Provinces:', error);
+      ctx.internalServerError('Failed to create Provinces');
+    }
+  }, async estimate_checked_p(ctx: Context) {
     const S_provinceData = ctx.request.body;
     try {
       const Get_Province = await strapi.entityService.findOne('api::province.province', S_provinceData.data.Province_id, {
         populate: {
-          districts: { 
+          districts: {
             populate: {
               sub_districts: {
                 populate: {
-                  customers: true, 
+                  customers: {
+                    populate: {
+                      business_with: true
+                    }
+                  }
                 },
               },
             },
@@ -132,31 +183,60 @@ export default {
           name_th: string;
           name_en: string;
           sub_districts: Array<{
-            id: string;
-            documentId: string;
-            name_th: string;
-            name_en: string;
-            customers: any[]; 
-          }>;
+            id: string,
+            documentId: string,
+            name_th: string,
+            name_en: string,
+            customers: Array<{
+              id: string,
+              documentId: string,
+              estimate: string,
+              business_with: {
+                id: string,
+                documentId: string
+              }
+            }>
+          }>
         }>;
       };
-      
+
       const allCustomers: any[] = [];
-      
-      Get_Province.districts.forEach(district => {
-        district.sub_districts.forEach(subDistrict => {
-          if (subDistrict.customers && subDistrict.customers.length > 0) {
-            allCustomers.push(...subDistrict.customers); 
-          }
+
+      if (S_provinceData.data.team_id === null) {
+        Get_Province.districts.forEach(district => {
+          district.sub_districts.forEach(subDistrict => {
+            if (subDistrict.customers && subDistrict.customers.length > 0) {
+              allCustomers.push(...subDistrict.customers);
+            }
+          });
         });
-      });
-  
+
+      } else {
+        const Get_team = await strapi.entityService.findOne('api::team.team', S_provinceData.data.team_id);
+        if (!Get_team) {
+          return ("ไม่เจอทีมอะคุณ Id ผิดป่าว")
+        }
+        Get_Province.districts.forEach(district => {
+          district.sub_districts.forEach(subDistrict => {
+            if (subDistrict.customers && subDistrict.customers.length > 0) {
+              subDistrict.customers.forEach(customer => {
+                console.log(customer.business_with)
+                if (customer.business_with && customer.business_with.id === Get_team.id) {
+                  allCustomers.push(customer);
+                }
+              });
+            }
+          });
+        });
+      }
+
+
       const statusCount = {
         green: 0,
         yellow: 0,
         red: 0,
       };
-  
+
       allCustomers.forEach(customer => {
         const estimateStatus = customer.estimate;
         if (estimateStatus.toLowerCase() === 'green') {
@@ -167,38 +247,43 @@ export default {
           statusCount.red++;
         }
       });
-  
+
       ctx.send({
         greenCount: statusCount.green,
         yellowCount: statusCount.yellow,
         redCount: statusCount.red,
+        total_customer: statusCount.green + statusCount.yellow + statusCount.red
       }, 200);
-  
+
     } catch (error) {
       console.error('Error fetching province data:', error);
       ctx.internalServerError('Failed to fetch province data');
     }
-  },async estimate_checked_r(ctx: Context) {
+  }, async estimate_checked_r(ctx: Context) {
     const S_RegionData = ctx.request.body;
+    console.log(S_RegionData.data.Region_id)
     try {
-      // ดึงข้อมูลจาก Region โดยใช้ Region_id
-      const Get_Region = await strapi.entityService.findOne('api::region.region', S_RegionData.data.Region_id, {
+      const Get_Region = await strapi.entityService.findMany('api::province.province', {
+        filters: {
+          geography_id: { name: S_RegionData.data.Region_id }
+        },
         populate: {
-          provinces: { // ดึงข้อมูลจาก Relation ของ Provinces
+          geography_id: true,
+          districts: {
             populate: {
-              districts: {
+              sub_districts: {
                 populate: {
-                  sub_districts: {
+                  customers: {
                     populate: {
-                      customers: true, // ดึงข้อมูลลูกค้าจาก sub_districts
-                    },
-                  },
+                      business_with: true
+                    }
+                  }
                 },
               },
             },
           },
         },
-      }) as {
+      }) as Array<{
         id: string;
         documentId: string;
         locale?: string;
@@ -207,49 +292,83 @@ export default {
         publishedAt?: string;
         name_en?: string;
         name_th?: string;
-        provinces: Array<{
+        geography_id: {
+          id: string;
+          documentId: string;
+          name: string;
+          createdAt: string;
+          updatedAt: string;
+        };
+        districts: Array<{
           id: string;
           documentId: string;
           name_th: string;
           name_en: string;
-          districts: Array<{
-            id: string;
-            documentId: string;
-            name_th: string;
-            name_en: string;
-            sub_districts: Array<{
-              id: string;
-              documentId: string;
-              name_th: string;
-              name_en: string;
-              customers: any[];
-            }>;
-          }>;
+          sub_districts: Array<{
+            id: string,
+            documentId: string,
+            name_th: string,
+            name_en: string,
+            customers: Array<{
+              id: string,
+              documentId: string,
+              estimate: string,
+              business_with: {
+                id: string,
+                documentId: string
+              }
+            }>
+          }>
         }>;
-      };
-  
+      }>;
+
+
       if (!Get_Region) {
         return ctx.badRequest('ไม่พบข้อมูล Region ที่ต้องการ');
       }
-  
+
       const allCustomers: any[] = [];
-  
-      Get_Region.provinces.forEach(province => {
-        province.districts.forEach(district => {
-          district.sub_districts.forEach(subDistrict => {
-            if (subDistrict.customers && subDistrict.customers.length > 0) {
-              allCustomers.push(...subDistrict.customers);
-            }
+
+
+      console.log(Get_Region)
+      if (S_RegionData.data.team_id === null) {
+        Get_Region.forEach(province => {
+          province.districts.forEach(district => {
+            district.sub_districts.forEach(subDistrict => {
+              if (subDistrict.customers && subDistrict.customers.length > 0) {
+                allCustomers.push(...subDistrict.customers);
+              }
+            });
           });
-        });
-      });
-  
+        })
+
+      } else {
+        const Get_team = await strapi.entityService.findOne('api::team.team', S_RegionData.data.team_id);
+        if (!Get_team) {
+          return ("ไม่เจอทีมอะคุณ Id ผิดป่าว")
+        }
+        Get_Region.forEach(province => {
+          province.districts.forEach(district => {
+            district.sub_districts.forEach(subDistrict => {
+              if (subDistrict.customers && subDistrict.customers.length > 0) {
+                subDistrict.customers.forEach(customer => {
+                  console.log(customer.business_with)
+                  if (customer.business_with && customer.business_with.id === Get_team.id) {
+                    allCustomers.push(customer);
+                  }
+                });
+              }
+            });
+          });
+        })
+      }
+
       const statusCount = {
         green: 0,
         yellow: 0,
         red: 0,
       };
-  
+
       allCustomers.forEach(customer => {
         const estimateStatus = customer.estimate;
         if (estimateStatus.toLowerCase() === 'green') {
@@ -260,34 +379,49 @@ export default {
           statusCount.red++;
         }
       });
-  
+
       ctx.send({
         greenCount: statusCount.green,
         yellowCount: statusCount.yellow,
         redCount: statusCount.red,
+        total_customer: statusCount.green + statusCount.yellow + statusCount.red
       }, 200);
-  
+
     } catch (error) {
       console.error('Error fetching region data:', error);
       ctx.internalServerError('Failed to fetch region data');
     }
   },
   async estimate_checked_all(ctx: Context) {
+    const S_districtData = ctx.request.body;
     try {
-      const allCustomers = await strapi.entityService.findMany('api::customer.customer');
-  
+      let allCustomers 
+      if (S_districtData.data.team_id !== null) {
+        const Get_team = await strapi.entityService.findOne('api::team.team', S_districtData.data.team_id);
+        if (!Get_team) {
+          return ("ไม่เจอทีมอะคุณ Id ผิดป่าว")
+        }
+        allCustomers = await strapi.entityService.findMany('api::customer.customer', {
+          filters: {
+            business_with: S_districtData.data.team_id
+          }
+        });
+      } else {
+        allCustomers = await strapi.entityService.findMany('api::customer.customer');
+      }
+
       const statusCount = {
         green: 0,
         yellow: 0,
         red: 0,
       };
-  
+
       allCustomers.forEach(customer => {
         const estimateStatus = customer.estimate;
 
         if (estimateStatus && typeof estimateStatus === 'string') {
           const status = estimateStatus.toLowerCase();
-          
+
           if (status === 'green') {
             statusCount.green++;
           } else if (status === 'yellow') {
@@ -297,21 +431,22 @@ export default {
           }
         }
       });
-  
+
       ctx.send({
         greenCount: statusCount.green,
         yellowCount: statusCount.yellow,
         redCount: statusCount.red,
+        total_customer: statusCount.green + statusCount.yellow + statusCount.red
       }, 200);
-  
+
     } catch (error) {
       console.error('Error fetching customer data:', error);
       ctx.internalServerError('Failed to fetch customer data');
     }
   }
-  
-  
-  
-  
-  
+
+
+
+
+
 };
